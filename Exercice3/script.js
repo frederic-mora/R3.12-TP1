fd7779389a892c6dce26c3242a43ef85dbfa0b8f
Mora Frederic <frederic.mora@unilim.fr>
2023-08-30 16:27:10 +0200

    Version initiale

---
+++ Exercice3/script.js
@@ -0,0 +1,25 @@
+/*
+   Objectif de l'exercice :
+   
+   . Savoir créer dynamiquement des éléments du DOM
+   . Savoir les configurer, définir leurs propriétés
+   . Savoir définir un lien parent-enfant entre deux éléments du DOM
+   . Savoir lier des éléments au DOM courant pour que le navigateur les fasse apparaître.
+  
+   En répondant correctement aux questions de cet exercice, vous devez obtenir un rendu analogue
+   au visuel objectif.jpg disponible dans le répertoire asset.
+
+   Comme d'habitude, interdiction d'éditez directement les fichiers index.html et style.css.
+   Seul le fichier script.js doit être modifié.
+
+*/
+
+
+/* Q1
+
+   Ecrire les instructions JS qui permettent d'obtenir la même page (et le même DOM)
+   que ce que vous voyez sur objectif.jpg du répertoire asset.
+   
+   Notez bien que c'est la partie droite d'objectif.jpg qui vous donne les bonnes informations !
+*/
+
---
+++ readme.txt
@@ -0,0 +1,7 @@
+R3.19 Développement front
+
+TP1
+
+Sélections et modifications simples d'éléments du DOM (éléments sélectionnables de manière directe)
+Gestion simple d'événements (traitement sur la cible de l'événement)
+
